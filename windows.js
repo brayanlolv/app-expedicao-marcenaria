b203b10e4e7adfbe90e072d9b15d62d8e3a90a4f
brayan <brayanlucasop@gmail.com>
2024-02-12 21:15:57 -0300

    estamos passando o id como parametro para a url

--- windows.js
+++ windows.js
@@ -1,5 +1,9 @@
-const {BrowserWindow} = require('electron')
+const {app,BrowserWindow} = require('electron');
+const path = require('path'); 
+
+const { request } = require('http');
 const { join } = require("path");
+const url = require('url')
 
 function pedidosWindow(arguments) {
     console.log(arguments.id)
@@ -9,10 +13,19 @@ function pedidosWindow(arguments) {
         webPreferences: {
             preload: join(__dirname, "preload.js"),
             sandbox: false,//uai ????   
-        },
-        globals: {id: 17}
+            additionalArguments: ["myvarvalue", "secondvarvalue", "--another=something"]
+        }
     })
-    win.loadFile('pages/details/pedidos-details.html')
+    // win.loadFile('pages/details/pedidos-details.html')
+    win.loadURL(url.format({
+        slashes: true,
+        protocol: 'file:',
+        pathname: path.resolve(app.getAppPath(), "pages/details/pedidos-details.html"),
+        query: {
+          test: 'test',
+          id:arguments.id
+        }
+      }));
 }
 
 module.exports = {pedidosWindow}
